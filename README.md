dbba2857700c1151e0b9c254e26dfddabd09dce5
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-11-29 12:02:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LegendGeSim.jl Tutorial
+
---
+++ make.jl
@@ -0,0 +1,6 @@
+using Weave
+
+# convert_doc("LegendGeSim_Tutorial.ipynb", "LegendGeSim_Tutorial.jmd")
+
+convert_doc("LegendGeSim_Tutorial.jmd", "LegendGeSim_Tutorial.jl")
+convert_doc("LegendGeSim_Tutorial.jmd", "LegendGeSim_Tutorial.ipynb")
